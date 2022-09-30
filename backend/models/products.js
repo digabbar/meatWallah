2e9c00a09ad9ab97b32225204281e71c2cdc0af7
“digabbar” <sohamjha.sara@gmail.com>
2022-09-30 20:26:22 +0530

    product model done

---
+++ backend/models/products.js
@@ -0,0 +1,77 @@
+const mongoose = require("mongoose");
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "please enter product name"],
+    trim: true,
+    maxLength: [100, "Product name cannot exceed 100 character"],
+  },
+  price: {
+    type: Number,
+    required: [true, "please enter product price"],
+    default: 0.0,
+  },
+  description: {
+    type: String,
+    required: [true, "please enter product description"],
+  },
+  ratings: {
+    type: Number,
+    default: 0,
+  },
+  image: [
+    {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  category: {
+    type: String,
+    required: [true, "Please enter category for this product"],
+    enums: {
+      values: ["Chickens", "Mutton", "Egg", "Fish"],
+      message: "Please select correct Category for product",
+    },
+  },
+  seller: {
+    type: String,
+    required: [true, "Please enter product Seller"],
+  },
+  stocks: {
+    type: Number,
+    required: [true, "please enter product stock"],
+    dafault: 0,
+  },
+  numOfReviews: {
+    type: Number,
+    dafault: 0,
+  },
+  reviews: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      rating: {
+        type: Number,
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);
