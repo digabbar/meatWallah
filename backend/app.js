df177db0ee58f444d160e599437fd02d02dff716
“digabbar” <sohamjha.sara@gmail.com>
2022-09-30 19:39:40 +0530

    First Route done

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,8 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
+// Import all Routes
+const products = require("./routes/product");
+
+app.use("/api/v1", products);
 module.exports = app;
