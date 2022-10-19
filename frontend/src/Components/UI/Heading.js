4ba63d5b1083ca39b0b46228dc456e731270d5e8
“digabbar” <sohamjha.sara@gmail.com>
2022-10-19 16:42:45 +0530

    rewritting frontend due to error

--- frontend/src/Components/UI/Heading.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import classes from "./Heading.module.css";
-const Heading = (props) => {
-  return <h1 className={classes.heading}>{props.name}</h1>;
-};
-
-export default Heading;
